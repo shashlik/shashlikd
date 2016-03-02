9e42f0a29fe2f184349d915e0f3e808c28470639
David Edmundson <kde@davidedmundson.co.uk>
2016-03-02 11:32:55 +0000

    Get APK using HTTP connection with host

--- shashlikd/java/com/example/david/shashlikssytem/BootReceiver.java
+++ shashlikd/java/com/example/david/shashlikssytem/BootReceiver.java
@@ -5,11 +5,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.os.SystemProperties;
+import android.os.StrictMode;
+import java.lang.Process;
+import java.lang.ProcessBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import android.net.Uri;
 
 import java.io.RandomAccessFile;
 import java.io.FileWriter;
 import java.io.OutputStreamWriter;
-import android.net.LocalSocket;
+import java.io.FileOutputStream;
+import java.io.File;
+
 
 public class BootReceiver extends BroadcastReceiver {
     public BootReceiver() {
@@ -21,26 +37,94 @@ public class BootReceiver extends BroadcastReceiver {
         MainApplication app = ((MainApplication) context.getApplicationContext());
         app.startServices();
 
-        //start boot app
-        Log.e("DAVE DAVE DAVE DAVE STARTUP", "Going to open socket");
+        //Make a HTTP request
+        //this does two things; fetches the name of the APK to run (bit overkill, when it could be a simple built prop)
+        //informs the desktop side we're loaded, incase it needs to send anything with a fully operation system
 
-        RandomAccessFile pipe;
+        //Android generally blocks network in the main thread, but we're only going to be showing a black screen idling anyway.
+        //may as well disable that setting
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        //Get and install the APK if needed
+        //if the APK doesn't exist the server will return a 403
+        //10.0.2.2 is a special qemu setup for localhost
         try {
-            pipe = new RandomAccessFile("/dev/qemu_pipe", "rw");
-            FileWriter x = new FileWriter(pipe.getFD());
-            x.write("pipe:unix:/tmp/asdf");
-            x.flush();
+            Log.d("Shashlikd", "Fetching APK");
+            URL url = new URL("http://10.0.2.2:60057/apk_file");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.connect();
+            int responseCode = conn.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // opens input stream from the HTTP connection
+                InputStream inputStream = conn.getInputStream();
 
-            Log.e("DAVE DAVE DAVE DAVE STARTUP", "opening pipe");
+                Log.d("Shashlikd", "Saving APK");
+                // opens an output stream to save into file
 
-            LocalSocket socket = new LocalSocket(pipe.getFD());
-            OutputStreamWriter socketStream = new OutputStreamWriter(socket.getOutputStream());
-            socketStream.write("CHEESE");
-            socketStream.flush();
+                File outputDir = context.getCacheDir();
+                File outputFile = File.createTempFile("apkSave", ".apk", outputDir);
+                outputFile.setReadable(true, false);
+                FileOutputStream outputStream = new FileOutputStream(outputFile);
 
-            Log.e("DAVE DAVE DAVE DAVE STARTUP", "DATA SENT");
-        } catch (Exception e){
-            Log.e("DAVE DAVE DAVE DAVE STARTUP", "could not open pipe");
+                int bytesRead = -1;
+                byte[] buffer = new byte[4096];
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+
+                outputStream.close();
+                inputStream.close();
+
+                Log.d("Shashlikd", "File downloaded");
+                Log.d("Shashlikd", outputFile.getPath());
+
+                Log.d("Shashlikd", "Installing APK");
+
+                Process result = new ProcessBuilder()
+                            .command("pm", "install", "-r", "-d", outputFile.getPath())
+                            .start();
+                result.waitFor();
+                Log.d("Shashlikd", "Apk install finished with " + result.exitValue());
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+
+        //Find the name of the APK to run and start the main activity
+        try {
+            Log.d("Shashlikd", "Getting APK ID");
+            URL url = new URL("http://10.0.2.2:60057/startup");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.connect();
+            InputStream inputStream = conn.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+            String apk = reader.readLine();
+
+            if (!apk.isEmpty()) {
+                //now try to start that APK
+                Log.d("Shashlikd", "STARTING");
+                Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(apk);
+                if (launchIntent != null) {
+                    context.startActivity(launchIntent);
+                } else {
+                    Log.e("Shashlikd", "APK not installed");
+                }
+            } else {
+                Log.w("Shashlikd", "No APK name returned");
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
